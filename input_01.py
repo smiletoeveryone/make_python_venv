fefa159f8fcba9aceb471ddb955a861bc5d1dc44
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2019-12-15 22:02:37 +0800

    Update input_01.py

--- input_01.py
+++ input_01.py
@@ -5,6 +5,6 @@ name = input("What is your name?\n")
 
 print("Nice to meet you, " + name)
 
-age =int(input("How old are you?\n"))
+age =int(input("How old are you?\n")) # you shoud make a type transform when you ask information what about numbers 
 age2 = age + 1
 print("So next year you will be %d years old!" % age2)
