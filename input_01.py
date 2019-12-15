17ad824daf9f1540adbe41e4b12842e2f5585408
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2019-12-15 21:59:52 +0800

    Add files via upload
    python input function

---
+++ input_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+name = input("What is your name?\n")
+
+print("Nice to meet you, " + name)
+
+age =int(input("How old are you?\n"))
+age2 = age + 1
+print("So next year you will be %d years old!" % age2)
