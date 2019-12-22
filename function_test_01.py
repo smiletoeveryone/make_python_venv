2c1721a9fa6cdc97ca2f86825b5d376da8622a85
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2019-12-22 13:03:20 +0800

    Add files via upload

---
+++ function_test_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+# def addition(num1, num2):
+num1 = int(input("please enter a number for num1\n"))
+num2 = int(input("please enter a number for num2\n"))
+    
+total = num1 + num2
+
+
+    
+print(total)
